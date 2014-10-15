4defe00122bf1f14307c658853f225a163b0011a
Lopaka Delp <lopaka@rightscale.com>
2014-10-15 12:37:08 -0700

    added metadata.rb

--- cookbooks/test_cookbook/metadata.rb
+++ cookbooks/test_cookbook/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures test_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe "test_cookbook::default", "test cookbook"
